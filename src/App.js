617d0df45e0679b303afef35842945256f9cf6b7
ajaychordiya <ajaychordiya01@gmail.com>
2022-08-16 22:38:23 +0530

    right sidebar done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import Sidebar from "./component/Sidebar";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
   return (
@@ -22,19 +23,26 @@ function App() {
             height="68"
             className="h-14 w-12"
           />
-          <h1 className="font-light text-2xl ml-3"> Bookiest .....</h1>
+          <h1 className="font-light text-2xl ml-3 text-[#0D276B]">
+            Bookiest .....
+          </h1>
         </div>
-        <input
-          type="text"
-          placeholder="Search book titles, authors, publishers..."
-          className="border-none rounded-lg bg-slate-200 w-4/12 h-10 px-4 active:border-sky-500"
-        />
-        <select className="w-60 h-10 mr-2 rounded-lg bg-slate-200 px-4 border-none">
+
+        <div className="rounded-lg bg-[#F0F4FF] w-4/12 h-10 px-4">
+          <SearchIcon className="text-[#0D276B] w-6 h-7 my-3" />
+          <input
+            type="text"
+            placeholder="Search book titles, authors, publishers..."
+            className="border-none bg-[#F0F4FF] w-11/12 px-1 active:border-none focus:outline-none"
+          />
+        </div>
+
+        <select className="w-72 h-10 mr-2 rounded-lg bg-[#F0F4FF] px-4 border-none">
           <option>select city</option>
         </select>
       </div>
       <div className="flex m-2">
-        <div className="left-sidebar flex flex-col justify-around flex-none">
+        <div className="left-sidebar flex flex-col justify-around flex-none px-2">
           <div className="top">
             <div>
               <DashboardIcon className="text-[#0D276B] w-6 h-6 my-3" />
@@ -66,7 +74,14 @@ function App() {
         </div>
         <div className="middle flex-auto w-80">aaa</div>
 
-        <div className="right-sidebar flex-initial bg-slate-200 w-60 mr-2 rounded-lg">
+        <div className="right-sidebar flex-initial bg-[#F0F4FF] w-72 mr-2 rounded-lg p-4">
+          <div className="flex justify-between pb-3">
+            <div className="text-[#1c1c1c]">Top Recommendation </div>
+            <div className="bg-[#FF1313] text-white rounded-full h-6 w-6 text-center">
+              3
+            </div>
+          </div>
+
           <Sidebar />
         </div>
       </div>
--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import book from "../static/images/book.png";
 
 let data = [
   {
@@ -25,7 +26,24 @@ const Sidebar = () => {
   return (
     <div>
       {data.map((d, index) => {
-        return <div key={index}> {d.title} </div>;
+        return (
+          <div key={index} className="bg-white rounded-lg">
+            <div className=" flex  mt-2  py-2 px-2">
+              <img
+                src={book}
+                alt="Logo"
+                width="58"
+                height="68"
+                className="h-14 w-12 rounded-lg"
+              />
+              <div className="ml-4">
+                <div className="text-base font-light"> {d.title} </div>
+                <div className="text-xs"> {d.author}</div>
+              </div>
+            </div>
+            <div className="text-sm px-2 py-1 ">{d.description}</div>
+          </div>
+        );
       })}
     </div>
   );
